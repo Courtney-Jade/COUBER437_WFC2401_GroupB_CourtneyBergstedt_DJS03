d86a7943e6f1c6e89557d12d505a2e9c8f39f9f8
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-05-17 00:23:43 +0200

    created function for renderBookList

--- COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS03-main/scripts.js
+++ COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS03-main/scripts.js
@@ -9,25 +9,23 @@ const app = {
  
  const starting = document.createDocumentFragment()
  
- for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-     const element = document.createElement('button')
-     element.classList = 'preview'
-     element.setAttribute('data-preview', id)
- 
-     element.innerHTML = `
-         <img
-             class="preview__image"
-             src="${image}"
-         />
-         
-         <div class="preview__info">
-             <h3 class="preview__title">${title}</h3>
-             <div class="preview__author">${authors[author]}</div>
-         </div>
-     `
- 
-     starting.appendChild(element)
- }
+function renderBookList(books, container) {
+    const fragment = document.createDocumentFragment();
+    for (const {author, id, image, title} of books) {
+        const element = document.createElement('button');
+        element.classList = 'preview';
+        element.setAttribute('data-preview', id);
+        element.innerHTML = `
+        <img class="preview__image" src="${image}" />
+        <div class="preview__info">
+            <h3 class="preview__title">${title}</h3>
+            <div class="preview__author">${app.authors[author]}</div>
+        </div>
+    `;
+      fragment.appendChild(element);
+    }
+    container.appendChild(fragment);
+}
  
  document.querySelector('[data-list-items]').appendChild(starting)
  
